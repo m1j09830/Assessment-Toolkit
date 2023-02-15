0de2c26269bec5bbc58d68aa5c4bb31ee32a224c
m1j09830 <miguel.rios@trio.dhs.gov>
2023-02-15 12:42:17 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Assessment-Toolkit
+
+
+## Install
+Assessment-Toolkit can be installed by cloning this repository and running pip3 install . and subsequently executed from PATH with Assessment-Toolkit
+
+## Usage
+
+## Examples
+
+## Development
+Assessment-Toolkit uses Poetry to manage dependencies. Install from source and setup for development with:
+```
+git clone https://github.com/m1j09830/Assessment-Toolkit
+cd Assessment-Toolkit
+poetry install
+poetry run Assessment-Toolkit --help
+```
+
+## Credits
---
+++ assessment_toolkit/__main__.py
@@ -0,0 +1,61 @@
+import typer
+from enum import Enum
+from pathlib import Path
+from assessment_toolkit.logger import init_logger, logger, console
+from assessment_toolkit import __version__
+from assessment_toolkit.lib.asmt_scans import *
+
+app = typer.Typer(
+    add_completion=False,
+    rich_markup_mode='rich',
+    context_settings={'help_option_names': ['-h', '--help']},
+    pretty_exceptions_show_locals=False
+)
+
+class Operations(str, Enum):
+    scoper = "scoper"
+    shodan = "shodan"
+    discovery = "discovery"
+    full = "full"
+    nikto = "nikto"
+    aquatone = "aquatone"
+    internal_scans = "internal_scans"
+    external_scans = "external_scans"
+    backup = "backup"
+
+@app.command(no_args_is_help=True, help='Assessment-Toolkit help!')
+def main(
+    operation: Operations = typer.Option(..., '--operation', '-o', 
+        help='Operation to be run'),
+    project_name: str = typer.Option(..., '--project-name', '-p', help='Set project name'),
+    input_file: Path = typer.Option(
+        ..., '--input-file', '-i', exists=True, file_okay=True, dir_okay=False, 
+        readable=True, help='Set input file'),
+    exclude_file: Path = typer.Option(
+        None, '--exclude-file', '-e', exists=True, file_okay=True, dir_okay=False, 
+        readable=True, help='Set input file'),
+    debug: bool = typer.Option(False, '--debug', help='Enable [green]DEBUG[/] output')):
+
+    init_logger(debug)
+
+    input_file=str(input_file)
+    exclude_file=str(exclude_file)
+
+    if operation == Operations.scoper:
+        scoper(project_name, input_file, exclude_file)
+    
+    if operation == Operations.discovery:
+        discovery(project_name, input_file)
+    
+    if operation == Operations.full:
+        full_port(project_name, input_file)
+
+    if operation == Operations.nikto:
+        nikto(project_name, input_file)
+
+    if operation == Operations.aquatone:
+        aquatone(project_name, input_file)
+
+
+if __name__ == '__main__':
+    app(prog_name='Assessment-Toolkit')
